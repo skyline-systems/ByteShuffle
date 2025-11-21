711a2c553d00fbdf3ae80556427df4e04dd8b856
Sahib <skylinesystems@Sahibs-MacBook-Air.local>
2025-11-21 04:21:22 -0500

    call supabase directly from server component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
+import { createClient } from "@/utils/supbase/server";
+
 export default async function Page() {
-  const data = await fetch(
-    `http://${
-      process.env.VERCEL_ENV === "development"
-        ? "localhost:3000"
-        : process.env.VERCEL_URL
-    }/api/random`
+  const supabase = await createClient();
+  const { data, error } = await supabase.from("stories").select("*").limit(20);
+  if (error || !data || data.length === 0) {
+    return (
+      <>
+        <h1>No story available</h1>
+      </>
+    );
+  }
+  const story = data[Math.floor(Math.random() * data.length)];
+
+  return (
+    <>
+      <h1>{process.env.VERCEL_ENV}</h1>
+      <h1>{process.env.VERCEL_URL}</h1>
+      <iframe src={story.url} width={600} height={500} />
+    </>
   );
-  const story = await data.json();
-  return <iframe src={story.url} width={600} height={500} />;
 }
