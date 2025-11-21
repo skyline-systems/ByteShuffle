afc14d85768f88e729177eb769aa49998386d004
Sahib <skylinesystems@Sahibs-MacBook-Air.local>
2025-11-21 04:07:53 -0500

    add dev/prod url condition

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 export default async function Page() {
-  const data = await fetch(`https://${process.env.VERCEL_URL}/api/random`);
+  const data = await fetch(
+    `http://${
+      process.env.VERCEL_ENV === "development"
+        ? "localhost:3000"
+        : process.env.VERCEL_URL
+    }/api/random`
+  );
   const story = await data.json();
-  console.log({ story });
   return <iframe src={story.url} width={600} height={500} />;
 }
